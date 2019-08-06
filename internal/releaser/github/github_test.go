bdc4fb1d747ac14dc6ec6e8780215e5a6c5f64b3
fwiedmann <privat@felixwiedmann.de>
2019-08-06 23:21:25 +0200

    tmp(releaser/github): add test for create release

--- internal/releaser/github/github_test.go
+++ internal/releaser/github/github_test.go
@@ -2,10 +2,15 @@ package github_test
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
+	"github.com/Masterminds/semver"
+
 	"github.com/Nightapes/go-semantic-release/internal/releaser/github"
+	"github.com/Nightapes/go-semantic-release/internal/shared"
 	"github.com/Nightapes/go-semantic-release/pkg/config"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,6 +18,15 @@ import (
 type testDouble struct {
 	config config.GitHubProvider
 	valid  bool
+}
+
+type testFourth struct {
+	config              config.GitHubProvider
+	releaseVersion      *shared.ReleaseVersion
+	generatedChangelog  *shared.GeneratedChangelog
+	requestResponseBody string
+	requestResponseCode int
+	valid               bool
 }
 
 var doublesNew = []testDouble{
@@ -53,6 +67,71 @@ var doublesValidateConfig = []testDouble{
 	},
 		valid: false,
 	},
+}
+
+var lastVersion, _ = semver.NewVersion("1.0.0")
+var newVersion, _ = semver.NewVersion("2.0.0")
+
+var fourthsReleas = []testFourth{
+	testFourth{
+		config: config.GitHubProvider{
+			Repo: "foo",
+			User: "bar",
+		},
+		releaseVersion: &shared.ReleaseVersion{
+			Last: shared.ReleaseVersionEntry{
+				Version: lastVersion,
+				Commit:  "foo",
+			},
+			Next: shared.ReleaseVersionEntry{
+				Version: newVersion,
+				Commit:  "bar",
+			},
+			Branch: "master",
+			Draft:  false,
+		},
+		generatedChangelog: &shared.GeneratedChangelog{
+			Title:   "title",
+			Content: "content",
+		},
+		requestResponseBody: "{  \"url\": \"https://api.github.com/repos/octocat/Hello-World/releases/1\",  \"html_url\": \"https://github.com/octocat/Hello-World/releases/v1.0.0\",  \"assets_url\": \"https://api.github.com/repos/octocat/Hello-World/releases/1/assets\",  \"upload_url\": \"https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets{?name,label}\",  \"tarball_url\": \"https://api.github.com/repos/octocat/Hello-World/tarball/v1.0.0\",  \"zipball_url\": \"https://api.github.com/repos/octocat/Hello-World/zipball/v1.0.0\",  \"id\": 1,  \"node_id\": \"MDc6UmVsZWFzZTE=\",  \"tag_name\": \"v1.0.0\",  \"target_commitish\": \"master\",  \"name\": \"v1.0.0\",  \"body\": \"Description of the release\",  \"draft\": false,  \"prerelease\": false,  \"created_at\": \"2013-02-27T19:35:32Z\",  \"published_at\": \"2013-02-27T19:35:32Z\",  \"author\": {    \"login\": \"octocat\",    \"id\": 1,    \"node_id\": \"MDQ6VXNlcjE=\",    \"avatar_url\": \"https://github.com/images/error/octocat_happy.gif\",    \"gravatar_id\": \"\",    \"url\": \"https://api.github.com/users/octocat\",    \"html_url\": \"https://github.com/octocat\",    \"followers_url\": \"https://api.github.com/users/octocat/followers\",    \"following_url\": \"https://api.github.com/users/octocat/following{/other_user}\",    \"gists_url\": \"https://api.github.com/users/octocat/gists{/gist_id}\",    \"starred_url\": \"https://api.github.com/users/octocat/starred{/owner}{/repo}\",    \"subscriptions_url\": \"https://api.github.com/users/octocat/subscriptions\",    \"organizations_url\": \"https://api.github.com/users/octocat/orgs\",    \"repos_url\": \"https://api.github.com/users/octocat/repos\",    \"events_url\": \"https://api.github.com/users/octocat/events{/privacy}\",    \"received_events_url\": \"https://api.github.com/users/octocat/received_events\",    \"type\": \"User\",    \"site_admin\": false  },  \"assets\": [  ]}",
+		requestResponseCode: 500,
+		valid:               true,
+	},
+	testFourth{
+		config: config.GitHubProvider{
+			Repo: "foo",
+			User: "bar",
+		},
+		releaseVersion: &shared.ReleaseVersion{
+			Last: shared.ReleaseVersionEntry{
+				Version: lastVersion,
+				Commit:  "foo",
+			},
+			Next: shared.ReleaseVersionEntry{
+				Version: newVersion,
+				Commit:  "bar",
+			},
+			Branch: "master",
+			Draft:  false,
+		},
+		generatedChangelog: &shared.GeneratedChangelog{
+			Title:   "title",
+			Content: "content",
+		},
+		requestResponseBody: "",
+		requestResponseCode: 422,
+		valid:               false,
+	},
+}
+
+func initHTTPServer(respCode int, body string) *httptest.Server {
+
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+
+		rw.Write([]byte(body))
+		rw.WriteHeader(500)
+	}))
 }
 
 func TestNew(t *testing.T) {
@@ -115,4 +194,31 @@ func TestValidateConfig(t *testing.T) {
 
 	}
 	os.Unsetenv("GITHUB_ACCESS_TOKEN")
+}
+
+func TestCreateRelease(t *testing.T) {
+	os.Setenv("GITHUB_ACCESS_TOKEN", "XX")
+
+	for _, testObejct := range fourthsReleas {
+		if testObejct.valid {
+			server := initHTTPServer(testObejct.requestResponseCode, "")
+			testObejct.config.CustomURL = server.URL
+			client, _ := github.New(&testObejct.config)
+
+			err := client.CreateRelease(testObejct.releaseVersion, testObejct.generatedChangelog)
+			assert.Equal(t, testObejct.valid, err == nil)
+
+		} else {
+			testObejct.config.CustomURL = "foo"
+			client, _ := github.New(&testObejct.config)
+
+			err := client.CreateRelease(testObejct.releaseVersion, testObejct.generatedChangelog)
+			if err != nil {
+				t.Log(err)
+			}
+			assert.Equal(t, testObejct.valid, err == nil)
+		}
+	}
+	os.Unsetenv("GITHUB_ACCESS_TOKEN")
+
 }
