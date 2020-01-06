575ba5d5bd0225dccfc814951648e1dbb2854be9
Nightapes <svbeisch@gmail.com>
2020-01-06 17:41:10 +0100

    fix(hooks): improve cmd execution

---
+++ cmd/go-semantic-release/commands/hooks.go
@@ -0,0 +1,67 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/Nightapes/go-semantic-release/internal/hooks"
+	"github.com/Nightapes/go-semantic-release/pkg/semanticrelease"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(hooksCmd)
+}
+
+var hooksCmd = &cobra.Command{
+	Use:   "hooks",
+	Short: "Run all hooks",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		config, err := cmd.Flags().GetString("config")
+		if err != nil {
+			return err
+		}
+
+		repository, err := cmd.Flags().GetString("repository")
+		if err != nil {
+			return err
+		}
+
+		force, err := cmd.Flags().GetBool("no-cache")
+		if err != nil {
+			return err
+		}
+
+		ignoreConfigChecks, err := cmd.Flags().GetBool("no-checks")
+		if err != nil {
+			return err
+		}
+
+		releaseConfig := readConfig(config)
+
+		s, err := semanticrelease.New(releaseConfig, repository, !ignoreConfigChecks)
+		if err != nil {
+			return err
+		}
+
+		provider, err := s.GetCIProvider()
+
+		if err != nil {
+			return err
+		}
+
+		releaseVersion, err := s.GetNextVersion(provider, force)
+		if err != nil {
+			return err
+		}
+
+		hook := hooks.New(releaseConfig, releaseVersion)
+
+		err = hook.PreRelease()
+		if err != nil {
+			return err
+		}
+
+		return hook.PostRelease()
+	},
+}
